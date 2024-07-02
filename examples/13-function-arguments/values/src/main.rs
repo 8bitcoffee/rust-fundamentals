755a7f2df845f035c73d49787e397e77d58d1223
J Read <j@8bit.coffee>
2024-07-02 16:17:54 -0500

    changed unit 13 to play with user input and vectors

--- examples/13-function-arguments/values/src/main.rs
+++ examples/13-function-arguments/values/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 
 fn sum(numbers: &[i32]) -> i32 {
     let mut result: i32 = 0;
@@ -7,9 +9,30 @@ fn sum(numbers: &[i32]) -> i32 {
     result
 }
 
+fn average(numbers: &[i32]) -> f64 {
+    let sum: f64 = sum(numbers) as f64;
+    let length: f64 = numbers.len() as f64;
+    sum / length
+}
+
+
 fn main() {
-    // There are no variadic arguments in Rust
-    let numbers = [1, 2, 3, 4, 5];
-    let result = sum(&numbers);
-    println!("The sum is {}", result);
+    let mut input: String = String::new();
+    println!("\nEnter the length of the array:");
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let length: usize = input.trim().parse().expect("Invalid input");
+
+    let mut numbers: Vec<i32> = Vec::new();
+    for i in 0..length {
+        let mut input: String = String::new();
+        println!("\nEnter number {} of {}:", i + 1, length);
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let number: i32 = input.trim().parse().expect("Invalid input");
+        numbers.push(number);
+    }
+
+    let result: i32 = sum(&numbers);
+    let average: f64 = average(&numbers);
+    println!("\nThe sum is {}", result);
+    println!("\nThe average is {}", average);
 }
